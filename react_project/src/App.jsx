5d5e72b5aeaf3e55f364160962421ec6f3c958d3
Kosta45 <konstanlebed8@gmail.com>
2024-02-10 21:03:15 +0100

    prepared node modules

---
+++ react_project/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Routes, Link, Route
+} from "react-router-dom";
+import Create from './components/Create';
+import Main from './components/Main';
+import About from './components/About';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Note from './components/Note';
+import Error from './components/Error';
+import './App.css';
+
+
+    <Route exact path="/" element={<Main/>}></Route>
+
+
+function App() {
+  return (
+    <div className="main">
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="/about" element={<About/>} />
+        <Route path="/create" element={<Create/>}  />
+        <Route path="/note" element={<Note/>}  />
+        <Route path="/note/:noteURL" element={<Note/>}  />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
