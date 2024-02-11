e478539f6ec3bd500f3cf9da2a6d150530f392a1
Kosta45 <konstanlebed8@gmail.com>
2024-02-11 20:04:07 +0100

    Created note element. And feth note to server

--- react_project/src/components/Note.jsx
+++ react_project/src/components/Note.jsx
@@ -1,6 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import env from '../env.json';
 
 
 const Note = () => {
+    let {noteURL} = useParams();
+    const [noteText, setNoteText] = useState('');
+
+
+    useEffect(() => {
+        if (noteURL !== undefined){
+            fetch(env.urlBackend, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded'
+                },
+                body: JSON.stringify({"url" : noteURL})
+            })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                if (response.result) {
+                    setNoteText(response.note)
+                }
+            })
+        }
+    }, []);
+
     return (
         <div></div>
     )
