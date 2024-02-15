9cdca4b639f035952b0862c04211cf7d54c056ef
Kosta45 <konstanlebed8@gmail.com>
2024-02-15 01:09:54 +0100

    getNote function for Note

--- react_project/src/components/Note.jsx
+++ react_project/src/components/Note.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import env from '../env.json';
 
@@ -6,6 +6,9 @@ import env from '../env.json';
 const Note = () => {
 
     let {noteURL} = useParams();
+
+    //refs
+    const urlInput = useRef();
 
     //states
     const [noteText, setNoteText] = useState('');
@@ -44,6 +47,19 @@ const Note = () => {
         }
     }, []);
 
+    const getNote = (event) => {
+        event.preventDefault();
+        let url = urlInput.current.value;
+        url = url.trim();
+        if (url === '') {
+            alert('Заполните поля');
+            return false;
+        }
+        noteURL = url;
+        window.location.href = env.url+'/'+url;
+
+    }
+
     return (
         <div>
         <div className={lineClass}>
@@ -52,6 +68,13 @@ const Note = () => {
         </div>
         <div className={errorClass}>
             <p>Произошла ошибка. Такой note не найден.</p>
+        </div>
+        <div className={formClass}>
+            <form action="" onSubmit={getNote}>
+              <label htmlFor="url">Введите hash заметки</label>
+              <input type="text" name="url" id="url" className="form-control" ref={urlInput}/>
+              <button type="submit" className="btn btn-primary">Искать Note</button>
+            </form>
         </div>
         </div>
     )
