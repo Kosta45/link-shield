88bde2b12482aaa1b5690c3ba4d7a0dfad00f0b2
Kosta45 <konstanlebed8@gmail.com>
2024-02-25 20:04:06 +0100

    Main container

--- react_project/src/components/Main.jsx
+++ react_project/src/components/Main.jsx
@@ -4,10 +4,10 @@
 const Main = () => {
     return (
         <div className="d-flex justify-content-center mt-5">
-            <div className="d-flex flex-column btn-group-vertical bg-white rounded"> 
-                  <a href="/create" className="btn btn-primary btn-lg border-0">Create note</a>
-                  <a href="/note" className="btn btn-primary btn-lg border-0">See note</a>
-            </div>
+             <div className="d-flex flex-column btn-group-vertical rounded test border border-3 border-white"> 
+                 <a href="/create" className="btn btn-primary btn-lg border-0">Create note</a>
+                 <a href="/note" className="btn btn-primary btn-lg border-0">See note</a>
+              </div>
         </div>
     )
 }
