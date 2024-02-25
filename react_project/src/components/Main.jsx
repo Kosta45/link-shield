ce3f7bbba84030e876069888cb9a6ad446051cde
Kosta45 <konstanlebed8@gmail.com>
2024-02-25 21:16:21 +0100

    .

--- react_project/src/components/Main.jsx
+++ react_project/src/components/Main.jsx
@@ -4,7 +4,7 @@
 const Main = () => {
     return (
         <div className="d-flex justify-content-center mt-5">
-             <div className="d-flex flex-column btn-group-vertical rounded test border border-3 border-white"> 
+             <div className="d-flex flex-column btn-group-vertical rounded main-btns border border-3 border-white"> 
                  <a href="/create" className="btn btn-primary btn-lg border-0">Create note</a>
                  <a href="/note" className="btn btn-primary btn-lg border-0">See note</a>
               </div>
